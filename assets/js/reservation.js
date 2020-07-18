28d2d8a3ce6f99e0716ddb1cc11af489b418f714
Raffay Ahmed <raf.amed97@gmail.com>
2020-07-18 12:57:03 -0700

    js files

---
+++ assets/js/reservation.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    
+    // Reserved Tables
+    $.ajax({
+        url:"/api/tables",
+        method:"GET"
+    }).then(function(response){
+        console.log(response);
+        for(i=0; i<response.length;i++){
+            var list = $("<li class='list-group-item'>");
+            list.append(
+                $("<h2>").text("Table " + (i + 1)),
+                $("<hr>"),
+                $("<h3>").text("ID: " + )
+                $("<h3>").text("Name: " + )
+                $("<h3>").text("Email: " + )
+                $("<h3>").text("Phone: " + )
+            );
+            $("#reservations").append(list);
+        }
+    });
+
+    $.ajax({
+        url:"/api/waitlist",
+        method:"GET",
+    }).then(function(response){
+        console.log(response);
+        for(i=0; i<response.length;i++){
+            var list = $("<li class='list-group-item'>");
+            list.append(
+                $("<h2>").text("Table " + (i + 1)),
+                $("<hr>"),
+                $("<h3>").text("ID: " + )
+                $("<h3>").text("Name: " + )
+                $("<h3>").text("Email: " + )
+                $("<h3>").text("Phone: " + )
+            );
+            $("#reservations").append(list);
+    })
+})
