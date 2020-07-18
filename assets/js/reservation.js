35f69fa8ea5486caf3f7a61ef6995cf709c5295e
Raffay Ahmed <raf.amed97@gmail.com>
2020-07-18 13:26:31 -0700

    js files

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -1,3 +1,5 @@
+const waitingArray = require("../../app/data/waitinglist-data");
+
 $(document).ready(function(){
     
     // Reserved Tables
@@ -30,11 +32,11 @@ $(document).ready(function(){
             list.append(
                 $("<h2>").text("Table " + (i + 1)),
                 $("<hr>"),
-                $("<h3>").text("ID: " + )
-                $("<h3>").text("Name: " + )
-                $("<h3>").text("Email: " + )
-                $("<h3>").text("Phone: " + )
+                $("<h3>").text("ID: " + waitingArray[i].customerID)
+                $("<h3>").text("Name: " + waitingArray[i].customerName)
+                $("<h3>").text("Email: " + waitingArray[i].customerEmail)
+                $("<h3>").text("Phone: " + waitingArray[i].phoneNumber)
             );
             $("#reservations").append(list);
-    })
+    }
 })
