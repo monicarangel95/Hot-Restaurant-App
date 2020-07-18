0d05eda8098671aa0c8cefb9931233a7ba246a02
Monica Rangel <joanna.monica@yahoo.com>
2020-07-18 11:38:09 -0700

    adding server files

--- server.js
+++ server.js
@@ -2,22 +2,14 @@
 var express = require ("express");
 var path = require("path");
 var tablelist = [];
-// Setting up express
-
-var express = require("express");
-var path = require("path");
-var tablelist = [];
-
-// Sets up the Express App
-// =============================================================
+// sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3001;
-
 // Handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
+// routes/////
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
@@ -40,37 +32,3 @@ app.get("/api/tables", function (req, res) {
 
 });
 
-// recieve current tables that are on the wait list
-app.get("/api/waitlist", function(req, res){
-  let tables = [];
-  if(tablelist.length > 5){
-    tables = tablelist.slice(5);
-  }
-  res.status(200).json(tables);
-})
-
-app.get("/api/cleartable", function(req, res){
-  tablelist = [];
-  res.status(200).json(tablelist);
-})
-
-// Create reserve post route
-app.post("/api/reserve", function (req, res) {
-  tablelist.push(
-    {
-      customerName: req.body.customerName || "",
-      phoneNumber: req.body.phoneNumber || "",
-      customerEmail: req.body.customerEmail || "",
-      customerID: req.body.customerID || ""
-    }
-  );
-  if(tablelist.length > 5){
-    res.status(200).send("You have been added to the wait list");
-  }else{
-    res.status(200).send("Your reservation has been accepted");
-  }
-});
-// Starts erver to begin listening
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
