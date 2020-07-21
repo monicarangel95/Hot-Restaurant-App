4d25de618edcfd30d8136babb1147e7f41ea2176
Monica Rangel <monicarangel@monicas-mbp-3.home>
2020-07-20 20:42:05 -0700

    adding file updates

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -1,42 +1,41 @@
-const waitingArray = require("../../app/data/waitinglist-data");
-
-$(document).ready(function(){
-    
-    // Reserved Tables
-    $.ajax({
-        url:"/api/tables",
-        method:"GET"
-    }).then(function(response){
-        console.log(response);
-        for(i=0; i<response.length;i++){
-            var list = $("<li class='list-group-item'>");
-            list.append(
-                $("<h2>").text("Table " + (i + 1)),
-                $("<hr>"),
-                $("<h3>").text("ID: " + )
-                $("<h3>").text("Name: " + )
-                $("<h3>").text("Email: " + )
-                $("<h3>").text("Phone: " + )
-            );
-            $("#reservations").append(list);
-        }
-    });
-
-    $.ajax({
-        url:"/api/waitlist",
-        method:"GET",
-    }).then(function(response){
-        console.log(response);
-        for(i=0; i<response.length;i++){
-            var list = $("<li class='list-group-item'>");
-            list.append(
-                $("<h2>").text("Table " + (i + 1)),
-                $("<hr>"),
-                $("<h3>").text("ID: " + waitingArray[i].customerID)
-                $("<h3>").text("Name: " + waitingArray[i].customerName)
-                $("<h3>").text("Email: " + waitingArray[i].customerEmail)
-                $("<h3>").text("Phone: " + waitingArray[i].phoneNumber)
-            );
-            $("#reservations").append(list);
-    }
-})
+// $(document).ready(function(){
+//     // Reserved Tables
+//     $.ajax({
+//         url:"/api/tables",
+//         method:"GET"
+//     }).then(function(response){
+//         console.log(response);
+//         console.log("****");
+//         for(var i = 0; i < response.length;i++){
+//             var tableList = $("tableList");
+//             var list = $("<li class='list-group-item>");
+//             list.append(
+//                 $("<h2>").text("Table " + (i + 1))),
+//                 $("<hr>"),
+//                 $("<h3>").text("ID: " + response[i].customerID),
+//                 $("<h3>").text("Name: " + response[i].customerName),
+//                 $("<h3>").text("Email: " + response[i].customerEmail),
+//                 $("<h3>").text("Phone: " + response[i].customerPhone),
+//             );
+//             tableList.append(list);
+//         }
+//     });
+// }
+//     $.ajax({
+//         url:"/api/waitlist",
+//         method:"GET",
+//     }).then(function(response){
+//         console.log(response);
+//         for(i=0; i < response.length;i++){
+//             var list = $("<li class='list-group-item>");
+//             list.append(
+//                 $("<h2>").text("Table " + (i + 1)),
+//                 $("<hr>"),
+//                 $("<h3>").text("ID: " + waitingArray[i].customerID)
+//                 $("<h3>").text("Name: " + waitingArray[i].customerName)
+//                 $("<h3>").text("Email: " + waitingArray[i].customerEmail)
+//                 $("<h3>").text("Phone: " + waitingArray[i].phoneNumber)
+//             );
+//             $("#reservations").append(list);
+//     }
+// })
--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,18 +1,18 @@
-$("#reserve").on("click", function(add){
-    //Creates Array of New Reservation Data
-    var newPeople = {
-        name: $("#name").val(),
-        number: $("#phonenum").val(),
-        email: $("#email").val(),
-        ID: $("groupID").val(),
-    };
+// $("#reserve").on("click", function(add){
+//     //Creates Array of New Reservation Data
+//     var newPeople = {
+//         name: $("#name").val(),
+//         number: $("#phonenum").val(),
+//         email: $("#email").val(),
+//         ID: $("groupID").val(),
+//     };
 
-    console.log(newPeople);
+//     console.log(newPeople);
 
-    $.post("/api/tables", newPeople, function(){
-        $("#name").val(""),
-        $("#phonenum").val(""),
-        $("#email").val(""),
-        $("groupID").val("")
-    })
-})
+//     $.post("/api/tables", newPeople, function(){
+//         $("#name").val(""),
+//         $("#phonenum").val(""),
+//         $("#email").val(""),
+//         $("groupID").val("")
+//     })
+// })
